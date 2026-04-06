3efcfce8553859c2eb77d82e87009d2f03405012
Matt <matthewwilshire1@gmail.com>
2026-04-06 17:50:09 +0100

    Convert tokens to word embeddings

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import numpy as np
+from transformers import GPT2Tokenizer
+
+class Model:
+    word_embeddings = np.fromfile("gpt2_weights/transformer_wte_weight.bin", dtype=np.float32).reshape(50257, 768)
+
+def embedding_layer(tokens):
+    """Simply convert the tokens to embeddings, just a simple lookup."""
+    return Model.word_embeddings[tokens]
+
 def main():
-    print("Hello from transformer-inference!")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokens = tokenizer("What the")['input_ids']
+
+    embeddings = embedding_layer(tokens)
+    print(embeddings.shape, embeddings)
 
 
 if __name__ == "__main__":
