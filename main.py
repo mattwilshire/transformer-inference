793e917dbd254669f60a3df67ccb5f35d15b156e
Matt <matthewwilshire1@gmail.com>
2026-04-06 18:10:49 +0100

    Add model constants

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 class Model:
-    word_embeddings = np.fromfile("gpt2_weights/transformer_wte_weight.bin", dtype=np.float32).reshape(50257, 768)
-    position_embeddings = np.fromfile("gpt2_weights/transformer_wpe_weight.bin", dtype=np.float32).reshape(1024, 768)
+    # ran gpt2_export to get these
+    VOCAB_SIZE = 50257
+    N_SEQ_LENGTH = 1024
+    EMB_DIM = 768
+    HEAD_DIM = 64
+    N_LAYER = 12
+    N_HEAD = 12
+    N_MLP_HIDDEN = 3072
+
+
+    word_embeddings = np.fromfile("gpt2_weights/transformer_wte_weight.bin", dtype=np.float32).reshape(VOCAB_SIZE, EMB_DIM)
+    position_embeddings = np.fromfile("gpt2_weights/transformer_wpe_weight.bin", dtype=np.float32).reshape(N_SEQ_LENGTH, EMB_DIM)
 
 def embedding_layer(tokens):
     """Simply convert the tokens to embeddings, just a simple lookup."""
@@ -20,6 +30,10 @@ def network(text):
     tokens = tokenizer(text)['input_ids']
     embeddings = embedding_layer(tokens)
     position_embeddings = positional_embedding_layer(embeddings)
+
+    for i in range(Model.N_LAYER):
+        print(i)
+
     print(position_embeddings)
 
 
@@ -27,7 +41,7 @@ def main():
     text = "What the"
     network(text)
     print()
-    gpt2(text)
+    # gpt2(text)
 
 
 def gpt2(text):
